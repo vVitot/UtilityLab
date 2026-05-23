93e1d745289175366c61259a478fcc02eabb8956
Vitor <davidvitor2011@gmail.com>
2026-05-23 18:12:04 -0300

    att

--- script.js
+++ script.js
@@ -50,7 +50,8 @@ function mostrarItem(indice, comandoId) {
   /*1-janelão*/['https://res.cloudinary.com/dmuatihuc/video/upload/v1779483755/v1_wquafm.mp4',
   /*2-ligação*/'https://res.cloudinary.com/dmuatihuc/video/upload/v1779483754/v2_pyxhjx.mp4',
   /*3-L*/'https://res.cloudinary.com/dmuatihuc/video/upload/v1779484373/v3_l2fdz8.mp4',
-  /*4-CT*/'https://res.cloudinary.com/dmuatihuc/video/upload/v1779484743/ct_pfgifc.mp4']
+  /*4-CT*/'https://res.cloudinary.com/dmuatihuc/video/upload/v1779484743/ct_pfgifc.mp4',
+ /*5-cabecinha*/'https://res.cloudinary.com/dmuatihuc/video/upload/v1779560995/cabecinha_c5xogd.mp4']
 
   imgPixel.src = `assets/img_pixel/p${indice}.png`
   video.src = urlVideo[indice - 1]
